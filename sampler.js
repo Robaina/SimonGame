cb852ed96fb76fd021ee8d5ca2a6a086bbfc7b6e
Semidán Robaina-Estévez <tintanjarafe@gmail.com>
2018-12-30 16:36:20 +0100

    update

--- sampler.js
+++ sampler.js
@@ -2,13 +2,13 @@
    Semidan Robaina Estevez
 */
 
-let drum1 = document.getElementById("drum1");
-let drum2 = document.getElementById("drum2");
-let plate1 = document.getElementById("plate1");
-let plate2 = document.getElementById("plate2");
+let Sound1 = document.getElementById("Sound1");
+let Sound2 = document.getElementById("Sound2");
+let Sound3 = document.getElementById("Sound3");
+let Sound4 = document.getElementById("Sound4");
 let chimp = document.getElementById("chimp");
 let fullScreen = false;
-let numberOfBits = 4;
+let numberOfBits = 6;
 let counter, button_sequence;
 let pressedStart = false;
 let buttonIDs = ["up", "down", "left", "right"];
@@ -21,40 +21,40 @@ for(let id of buttonIDs) {
   buttons[id] = elem;
 }
 
-function playDrum1() {
+function playSound1() {
   buttons["up"].style.filter = "brightness(140%)";
-  drum1.currentTime = 0;
-  drum1.play();
+  Sound1.currentTime = 0;
+  Sound1.play();
   setTimeout(function() {
     buttons["up"].style.filter = "brightness(100%)";
   }, 500);
 }
-function playDrum2() {
-  buttons["down"].style.filter = "brightness(140%)";
-  drum2.currentTime = 0;
-  drum2.play();
+function playSound2() {
+  buttons["down"].style.filter = "brightness(250%)";
+  Sound2.currentTime = 0;
+  Sound2.play();
   setTimeout(function() {
     buttons["down"].style.filter = "brightness(100%)";
   }, 500);
 }
-function playPlate1() {
-  buttons["left"].style.filter = "brightness(140%)";
-  plate1.currentTime = 0;
-  plate1.play();
+function playSound3() {
+  buttons["left"].style.filter = "brightness(250%)";
+  Sound3.currentTime = 0;
+  Sound3.play();
   setTimeout(function() {
     buttons["left"].style.filter = "brightness(100%)";
   }, 500);
 }
-function playPlate2() {
-  buttons["right"].style.filter = "brightness(140%)";
-  plate2.currentTime = 0;
-  plate2.play();
+function playSound4() {
+  buttons["right"].style.filter = "brightness(250%)";
+  Sound4.currentTime = 0;
+  Sound4.play();
   setTimeout(function() {
     buttons["right"].style.filter = "brightness(100%)";
   }, 500);
 }
 
-let playSound = {"up": playDrum1, "down": playDrum2, "left": playPlate1, "right": playPlate2};
+let playSound = {"up": playSound1, "down": playSound2, "left": playSound3, "right": playSound4};
 
 // Select a sequence randomly
 function playSoundSequence() {
@@ -94,7 +94,7 @@ function checkOrder(event) {
     } else {
       pressedStart = false;
       console.log(correct_counts, numberOfBits);
-      if (correct_counts === numberOfBits) {plate2.play()}
+      if (correct_counts === numberOfBits) {Sound4.play()}
     }
 
 
